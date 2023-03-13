ece01ddf98772ef46bff8695d93f3ef80930be5c
Franco Juri <francojuri22@gmail.com>
2023-03-12 21:31:53 -0300

    section added

--- app.js
+++ app.js
@@ -13,3 +13,21 @@ function onModuleText(item){
     module.classList.toggle('active');
     module.classList.contains('active') ? icon.textContent = 'remove' : icon.textContent = 'add';
 }
+
+function getRemainTime(deadline){
+    let now = new Date(),
+        remainTime = (new Date(deadline) - now + 1000) / 1000,
+        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2);
+    
+    return {
+        remainTime,
+        remainSeconds,
+        remainMinutes, 
+        remainHours,
+    }
+        
+}
+
+console.log(new Date().getTime());
